b6be92c8d7d6bdc64206e887c3e8c46ff0b1e50c
GyuriToth <70635390+GyuriToth@users.noreply.github.com>
2026-03-15 22:52:18 +0100

    pipeline init

--- main.tf
+++ main.tf
@@ -22,3 +22,16 @@ resource "aws_s3_bucket" "elso_bucketem" {
     Environment = "Dev"
   }
 }
+
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "go-app-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
