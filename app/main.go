b0572385f9ba0b0ba99f14eb85afcc095647ff37
GyuriToth <70635390+GyuriToth@users.noreply.github.com>
2026-03-16 20:41:31 +0100

    add tests to workflow

--- app/main.go
+++ app/main.go
@@ -12,12 +12,14 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		hostname, _ := os.Hostname()
-		fmt.Fprintf(w, "Hello! This is a Go application running in a container.\n")
-		fmt.Fprintf(w, "Server ID: %s\n", hostname)
-	})
+	http.HandleFunc("/", HandleRoot)
 
 	fmt.Printf("server started on port %s...\n", port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	hostname, _ := os.Hostname()
+	fmt.Fprintf(w, "Hello! This is a Go application running in a container.\n")
+	fmt.Fprintf(w, "Server ID: %s\n", hostname)
 }
